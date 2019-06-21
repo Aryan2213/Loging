44f4edef1c1a62ee78c5ef4c7d2d0e9bb9e9439e
Aryan <48577295+Aryan2213@users.noreply.github.com>
2019-06-21 18:12:32 +1000

    add loging system

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public GameObject loging;
+    public GameObject create;
+    public GameObject resetPassword;
+    public GameObject passwordConfig;
+
+    public InputField usernameL;
+    public InputField passwordL;
+
+
+    public InputField usernameC;
+    public InputField passwordC;
+    public InputField emailC;
+
+    public InputField emailR;
+
+    public InputField code;
+    public InputField userNameConfig;
+    public InputField updatePsassword;
+
+    public string resetCode;
+
+    private void Start()
+    {
+        Loging();
+    }
+
+    public void Loging()
+    {
+        loging.SetActive(true);
+        create.SetActive(false);
+        resetPassword.SetActive(false);
+        passwordConfig.SetActive(false);
+
+
+    }
+    public void Create()
+    {
+        loging.SetActive(false);
+        create.SetActive(true);
+        resetPassword.SetActive(false);
+        passwordConfig.SetActive(false);
+
+    }
+    public void ResetPasswordB()
+    {
+        loging.SetActive(false);
+        create.SetActive(false);
+        resetPassword.SetActive(true);
+        passwordConfig.SetActive(false);
+
+    }
+    public void passwordConfing()
+    {
+        loging.SetActive(false);
+        create.SetActive(false);
+        resetPassword.SetActive(false);
+        passwordConfig.SetActive(true);
+    }
+
+
+    public void SignInClicked()
+    {
+
+        var PHPResultString = DBContext.Login(usernameL.text, passwordL.text);
+        Debug.Log("I Loging");
+    }
+    public void CreateUser()
+    {
+        var PHPResultString = DBContext.CreateUser(usernameC.text, passwordC.text, emailC.text);
+        Debug.Log("I Create");
+    }
+    public void ResetPassword()
+    {
+        var usersUsername = DBContext.GetUsernameFromEmail(emailR.text);
+        Debug.Log(usersUsername);
+
+        resetCode = WebFunctions.Generate6CharResetCode();
+
+        WebFunctions.SendResetEmail(emailR.text, usersUsername, resetCode);
+
+        // Tkae them to the next screen
+
+    }
+    public void UpdatePassword()
+    {
+
+        if (resetCode == code.text)
+        {
+            var PHPResultString = DBContext.ResetPassword(userNameConfig.text, updatePsassword.text);
+        }
+
+
+    }
+
+}
+
